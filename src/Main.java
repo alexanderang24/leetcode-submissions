c90d8824e4132ff2fb55045e1b0a9bef957ea865
Alexander Ang <alexander@doku.com>
2026-01-02 14:11:49 +0700

    Initiate project: add Main and Solution class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    private static final Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        var result = solution.sum(1, 2);
+        System.out.println("result: " + result);
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,9 @@
+/**
+ * copy and paste Solution class from /problems folder here and call it on the Main class to run it.
+ */
+public class Solution {
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
