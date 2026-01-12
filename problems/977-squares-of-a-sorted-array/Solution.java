3981ad6859a1adc7bc2471fbc0bf802dc3de0b89
Alexander Ang <alexanderang.24@gmail.com>
2026-01-12 21:48:26 +0700

    add solution for problem 977

---
+++ problems/977-squares-of-a-sorted-array/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
