a244b3fbc4637ad6d876dd3e7aa06083488a6931
Alexander Ang <alexanderang.24@gmail.com>
2026-01-25 17:01:32 +0700

    Time: 1 ms (45.54%) | Memory: 47 MB (40.84%) - LeetSync

--- problems/941-sort-array-by-parity/sort-array-by-parity.java
+++ problems/941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int odd = 0; // to track odd value index to be swapped
+        int swap = 0; // to track index to be swapped
         int temp = -1;
         for (int i = 0; i < nums.length; i++) {
-            // if value is even and odd is found, swap
+            // if value is even, throw it to the front
             if (nums[i] % 2 == 0) {
                 temp = nums[i];
-                nums[i] = nums[odd];
-                nums[odd] = temp;
-                odd++;
+                nums[i] = nums[swap];
+                nums[swap] = temp;
+                swap++;
             }
         }
         return nums;
