f8fd22c29c22b85be01065737c9a37973c278852
Alexander Ang <alexanderang.24@gmail.com>
2026-01-30 20:46:48 +0700

    Time: 7 ms (37.81%) | Memory: 45 MB (58.72%) - LeetSync

---
+++ problems/414-third-maximum-number/third-maximum-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        // intuition:
+        // sort the array, loop from behind
+        // use counter to get the third distinct digit, else return nums[length-1]
+
+        Arrays.sort(nums);
+        // System.out.println("nums: " +  Arrays.toString(nums));
+        
+        int counter = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // System.out.println("nums["+i+"]: " + nums[i]);
+            // System.out.println("counter: " + counter);
+            
+            if (i == nums.length - 1 || nums[i] != nums[i+1]) {
+                counter++;
+            }
+
+            if (counter == 3) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
