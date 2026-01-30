44e716236b9bd1252ac1a3885d7155ed1354328c
Alexander Ang <alexanderang.24@gmail.com>
2026-01-30 20:54:59 +0700

    Time: 0 ms (100.00%) | Memory: 45.8 MB (27.50%) - LeetSync

--- problems/414-third-maximum-number/third-maximum-number.java
+++ problems/414-third-maximum-number/third-maximum-number.java
@@ -24,11 +24,15 @@ class Solution {
     }
     
     static {
-Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-    try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-            fw.write("0");
-        } catch (Exception e) {
-        }
-    }));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                    fw.write("0");
+            } catch (Exception e) {
+            }
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_memory.txt")) {
+                    fw.write("0");
+            } catch (Exception e) {
+            }
+        }));
     }
 }
