50c8d8894c4ae05c73187433ed2d3bf818573205
Alexander Ang <alexanderang.24@gmail.com>
2026-02-05 23:47:17 +0700

    Time: 0 ms (100.00%) | Memory: 43.6 MB (17.58%) - LeetSync

---
+++ problems/66-plus-one/plus-one.java
@@ -0,0 +1,19 @@
+import java.math.BigDecimal;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+            if (i == 0) {
+                break;
+            }
+        }
+        int[] spcDigits = new int[digits.length+1];
+        spcDigits[0] = 1;
+        return spcDigits;
+    }
+}
