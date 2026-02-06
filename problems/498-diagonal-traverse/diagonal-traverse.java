d747d7ec3c4e4b1bdae6963394e3b6eba32091a9
Alexander Ang <alexanderang.24@gmail.com>
2026-02-06 23:13:03 +0700

    Time: 3 ms (74.20%) | Memory: 48.1 MB (61.48%) - LeetSync

--- problems/498-diagonal-traverse/diagonal-traverse.java
+++ problems/498-diagonal-traverse/diagonal-traverse.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int fullLength = mat.length * mat[0].length;
-        int[] result = new int[fullLength];
+        int maxRow = mat.length;
+        int maxColumn = mat[0].length;
+        int[] result = new int[maxRow * maxColumn];
         int i = 0, j = 0;
         boolean goingUpRight = true;
 
-        for (int k = 0; k < fullLength; k++) {
+        for (int k = 0; k < maxRow * maxColumn; k++) {
             // System.out.println("k,i,j " + k + " " + i + " " + j + " mat: " + mat[i][j] + " goingUpRight: " + goingUpRight);
             result[k] = mat[i][j];
             
             if (goingUpRight) {
-                if (j == mat[0].length - 1) {
+                if (j == maxColumn - 1) {
                     i++;
                     goingUpRight = false;
                 } else if (i == 0) {
@@ -21,7 +22,7 @@ class Solution {
                     j++;
                 }
             } else { // going downleft
-                if (i == mat.length - 1) {
+                if (i == maxRow - 1) {
                     j++;
                     goingUpRight = true;
                 } else if (j == 0) {
