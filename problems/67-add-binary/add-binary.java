69d2ec2b29efe3cd931d28ed825ab306ba692653
Alexander Ang <alexanderang.24@gmail.com>
2026-02-15 15:12:45 +0700

    Time: 6 ms (7.22%) | Memory: 44 MB (18.66%) - LeetSync

--- problems/67-add-binary/add-binary.java
+++ problems/67-add-binary/add-binary.java
@@ -1,10 +1,7 @@
 import java.math.BigInteger;
 
 class Solution {
-    public String addBinary(String a, String b) {
-        BigInteger val1 = new BigInteger(a, 2); 
-        BigInteger val2 = new BigInteger(b, 2);
-        BigInteger sum = val1.add(val2);        
-        return sum.toString(2);
+    public String addBinary(String a, String b) {  
+        return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
     }
 }
