2292131bdea43c59d944017924cfa9b71299932b
Alexander Ang <alexanderang.24@gmail.com>
2026-02-16 17:50:44 +0700

    Time: 1 ms (45.36%) | Memory: 43 MB (69.35%) - LeetSync

--- problems/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ problems/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,12 +1,14 @@
 class Solution {
     public int strStr(String haystack, String needle) {
         int counter, idx;
+        int hlen = haystack.length();
+        int nlen = needle.length();
         
-        for (int i = 0; i < haystack.length() && i + needle.length() <= haystack.length(); i++) {
+        for (int i = 0; i < hlen && i + nlen <= hlen; i++) {
             idx = i;
             counter = 0;
             
-            for (int j = 0; j < needle.length()  && idx < haystack.length(); j++, idx++) {
+            for (int j = 0; j < nlen  && idx < hlen; j++, idx++) {
                 // System.out.println("idx" + idx + " " + haystack.charAt(idx) + ", j" + j + " " + needle.charAt(j));
                 // System.out.println("counter " + counter);
 
@@ -16,7 +18,7 @@ class Solution {
                 }
 
                 counter++;
-                if (counter == needle.length()) {
+                if (counter == nlen) {
                     // System.out.println("match, return");
                     return i;
                 }
