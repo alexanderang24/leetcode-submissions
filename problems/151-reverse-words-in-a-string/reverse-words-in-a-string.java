1d5e468c50ba1e06684cbf468e56f7f6551776f0
Alexander Ang <alexanderang.24@gmail.com>
2026-07-05 13:31:10 +0700

    Time: 33 ms (5.25%) | Memory: 50.9 MB (5.14%) - LeetSync

--- problems/151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ problems/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,19 +1,64 @@
 class Solution {
     public String reverseWords(String s) {
-        s = new StringBuilder(s).reverse().toString();
-        String[] words = s.split(" ");
-        String result = "";
-        for (int i = 0; i < words.length; i++) {
-            if (!words[i].isBlank()) {
-                result += new StringBuilder(words[i]).reverse().toString() + " ";
-            }
+        char[] chars = reverseAll(s);
+        reverseWords(chars);
+        return cleanSpaces(chars, chars.length);
+    }
+
+    public char[] reverseAll(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            char temp = chars[left];
+            chars[left++] = chars[right];
+            chars[right--] = temp;
         }
-        return result.trim();
+        // System.out.println("reversed string: " + new String(chars));
+        return chars;
+    }
+
+    public void reverseWords(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            while (i < chars.length && chars[i] == ' ') {
+                i++;
+            }
+
+            int left = i;
+            int right = i;
+            while (right < chars.length && chars[right] != ' ') {
+                right++;
+            }
+            int wordEnd = right - 1;
+            System.out.println("i=" + i + " left=" + left + " wordEnd=" + wordEnd);
+            i = right;
+
+            while (left < wordEnd) {
+                char temp = chars[left];
+                chars[left] = chars[wordEnd];
+                chars[wordEnd] = temp;
+                left++;
+                wordEnd--;
+            }
+            // System.out.println("after loop: " + new String(chars));
+        }
+    }
+
+    public String cleanSpaces(char[] a, int n) {
+        int i = 0, j = 0;
+
+        while (j < n) {
+            while (j < n && a[j] == ' ')
+                j++; // skip spaces
+            while (j < n && a[j] != ' ')
+                a[i++] = a[j++]; // keep non spaces
+            while (j < n && a[j] == ' ')
+                j++; // skip spaces
+            if (j < n)
+                a[i++] = ' '; // keep only one space
+        }
+
+        return new String(a).substring(0, i);
     }
 }
-
-/**
-the sky is blue
-eulb si yks eht
-blue is sky the
- */
