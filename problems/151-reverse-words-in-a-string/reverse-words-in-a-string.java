fc6ac382403228903d129b7a2c075fb84421ae5d
Alexander Ang <alexanderang.24@gmail.com>
2026-07-05 11:45:34 +0700

    Time: 11 ms (11.32%) | Memory: 46.8 MB (13.45%) - LeetSync

--- problems/151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ problems/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,12 +1,19 @@
 class Solution {
     public String reverseWords(String s) {
+        s = new StringBuilder(s).reverse().toString();
         String[] words = s.split(" ");
         String result = "";
-        for (int i = words.length - 1; i >= 0; i--) {
+        for (int i = 0; i < words.length; i++) {
             if (!words[i].isBlank()) {
-                result = result + words[i] + " ";
+                result += new StringBuilder(words[i]).reverse().toString() + " ";
             }
         }
         return result.trim();
     }
 }
+
+/**
+the sky is blue
+eulb si yks eht
+blue is sky the
+ */
