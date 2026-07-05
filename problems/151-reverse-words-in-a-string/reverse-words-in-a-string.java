f804bc48d19eeb346aae368466769277c7a49fbb
Alexander Ang <alexanderang.24@gmail.com>
2026-07-05 11:25:39 +0700

    Time: 9 ms (23.23%) | Memory: 47.3 MB (5.14%) - LeetSync

---
+++ problems/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        String result = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (!words[i].isBlank()) {
+                result = result + words[i] + " ";
+            }
+        }
+        return result.trim();
+    }
+}
