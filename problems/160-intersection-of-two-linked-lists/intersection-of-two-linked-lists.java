7b106dcbb4c9a5b6f14b7acecd1f8e79bb8e2455
Alexander Ang <alexanderang.24@gmail.com>
2026-07-14 23:03:09 +0700

    Time: 2 ms (50.49%) | Memory: 53.1 MB (12.50%) - LeetSync

--- problems/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ problems/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -14,35 +14,19 @@ public class Solution {
         ListNode listA = headA;
         ListNode listB = headB;
 
-        while (listA.next != null && listB.next != null) {
-            listA = listA.next;
-            listB = listB.next;
-        }
+        while (listA != listB) {
+            if (listA == null) {
+                listA = headB;
+            } else {
+                listA = listA.next;
+            }
 
-        while (listA.next != null) {
-            listA = listA.next;
-            headA = headA.next;
-        }
-        while (listB.next != null) {
-            listB = listB.next;
-            headB = headB.next;
-        }
-        // System.out.println("headA " + headA.val);
-        // System.out.println("headB " + headB.val);
-
-        if (headA == headB) {
-            return headA;
-        }
-
-        while (headA.next != null && headB.next != null) {
-            headA = headA.next;
-            headB = headB.next;
-            if (headA == headB) {
-                // System.out.println("return " + headA);
-                return headA;
+            if (listB == null) {
+                listB = headA;
+            } else {
+                listB = listB.next;
             }
         }
-        // System.out.println("return null");
-        return null;
+        return listA;
     }
 }
