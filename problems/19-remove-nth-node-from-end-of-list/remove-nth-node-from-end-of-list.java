6991b4402c9223a8cf30aa121f959f74b5e0b9dd
Alexander Ang <alexanderang.24@gmail.com>
2026-07-16 22:04:14 +0700

    Time: 0 ms (100.00%) | Memory: 43.7 MB (6.75%) - LeetSync

--- problems/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ problems/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -23,26 +23,23 @@ class Solution {
             }
             b = b.next;
             counter++;
-            System.out.println("counter " + counter);
-            System.out.println("a now at " + a.val);
-            System.out.println("b now at " + b.val);
+            // System.out.println("counter " + counter);
+            // System.out.println("a now at " + a.val);
+            // System.out.println("b now at " + b.val);
         }
 
         if (counter < n) {
             head = a.next;
         } else if (a.next == null) {
-            System.out.println("a next is null");
+            // System.out.println("a next is null");
             return null;
         } else if (a.next.next == null) {
-            System.out.println("a next next is null");
+            // System.out.println("a next next is null");
             a.next = null;
         } else {
-            System.out.println("deleting a.next and connecting a to a.next.next");
-            ListNode temp = a;
-            a = a.next.next;
-            temp.next = a;
+            // System.out.println("deleting a.next and connecting a to a.next.next");
+            a.next = a.next.next;
         }
-
         return head;
     }
 }
