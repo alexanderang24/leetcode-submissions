4bf6d91e16cd3932f292a6c9d35875d1587835f7
Alexander Ang <alexanderang.24@gmail.com>
2026-08-18 14:06:10 +0700

    Time: 0 ms (100.00%) | Memory: 46.7 MB (17.52%) - LeetSync

--- problems/328-odd-even-linked-list/odd-even-linked-list.java
+++ problems/328-odd-even-linked-list/odd-even-linked-list.java
@@ -18,19 +18,14 @@ class Solution {
         ListNode evenHead = even;
 
         while (odd.next != null && even.next != null) {
-            if (odd.next.next != null) {
-                // System.out.println("odd connecting " + odd.val + " to " + odd.next.next.val);
-                odd.next = odd.next.next;
-                odd = odd.next;
-            }
-            if (even.next.next != null) {
-                // System.out.println("even connecting " + even.val + " to " + even.next.next.val);
-                even.next = even.next.next;
-                even = even.next;
-            }
+            // System.out.println("odd connecting " + odd.val + " to " + odd.next.next.val);
+            odd.next = even.next;
+            odd = odd.next;
+            // System.out.println("even connecting " + even.val + " to " + even.next.next.val);
+            even.next = odd.next;
+            even = even.next;
         }
         odd.next = evenHead;
-        even.next = null;
         // System.out.println("connecting " + odd.val + " to " + evenHead.val);
         return head;
     }
