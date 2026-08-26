fa963ad6d444a65d7397f8dba8344e5446698c75
Alexander Ang <alexanderang.24@gmail.com>
2026-08-26 12:14:03 +0700

    Time: 0 ms (100.00%) | Memory: 43.8 MB (98.42%) - LeetSync

--- problems/21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ problems/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -12,27 +12,6 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode merged = new ListNode();
         ListNode head = merged;
-        // if (list1.val < list2.val) {
-        //     // System.out.println("use1");
-        //     merged = list1;
-        //     list1 = list1.next;
-        //     head = merged;
-        // } else if (list2.val < list1.val) {
-        //     // System.out.println("use2");
-        //     merged = list2;
-        //     list2 = list2.next;
-        //     head = merged;
-        // } else {
-        //     // System.out.println("same");
-        //     merged = list1;
-        //     list1 = list1.next;
-        //     head = merged;
-
-        //     merged.next = list2;
-        //     list2 = list2.next;
-
-        //     merged = merged.next;
-        // }
 
         while (list1 != null && list2 != null) {
             // System.out.println("processing 1 | 2: " + list1.val + " | " + list2.val);
@@ -55,14 +34,7 @@ class Solution {
             }
             merged = merged.next;
         }
-
-        if (list1 != null) {
-            // System.out.println("add remaining 1");
-            merged.next = list1;
-        } else if (list2 != null) {
-            // System.out.println("add remaining 2");
-            merged.next = list2;
-        }
+        merged.next = list1 != null ? list1 : list2;
         return head.next;
     }
 }
