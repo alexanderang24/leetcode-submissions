323f88f7e6e8b07bf0fc6936f47c8d66990069ad
Alexander Ang <alexanderang.24@gmail.com>
2026-08-29 17:36:30 +0700

    Time: 0 ms (100.00%) | Memory: 44.5 MB (10.03%) - LeetSync

--- problems/61-rotate-list/rotate-list.java
+++ problems/61-rotate-list/rotate-list.java
@@ -11,44 +11,32 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || k == 0 || head.next == null) {
-            // System.out.println("return immediately");
             return head;
         }
 
-        ListNode curr = head;
         ListNode tail = head;
         int length = 1;
 
-        while (k > 0 && tail.next != null) {
+        while (tail.next != null) { // move tail to end and count length
             tail = tail.next;
-            k--;
             length++;
         }
-        
-        if (k != 0) {
-            k = (k-1) % length;
-            if (k == 0) {
-                // System.out.println("rotation will end up the same, return head");
-                return head;
-            }
-            // System.out.println("k more than length, real k: " + k);
-            tail = head;
-            while (k > 0) {
-                tail = tail.next;
-                k--;
-            }
+
+        k = k % length; // count real k (number of times we actually have to rotate the list)
+        if (k == 0) { // rotation will ended up the same, return head
+            return head;
         }
 
-        while (tail.next != null) {
-            tail = tail.next;
+        // move curr position to new tail
+        ListNode curr = head;
+        for (int i = 0; i < length - k - 1; i++) {
             curr = curr.next;
         }
-        // System.out.println("tail ends at " + tail.val);
-        // System.out.println("curr ends at " + tail.val);
+
+        // connect tail to new head and make curr the tail
         ListNode newHead = curr.next;
         curr.next = null;
         tail.next = head;
-
         return newHead;
     }
 }
