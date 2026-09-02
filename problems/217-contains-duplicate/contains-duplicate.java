5cb3cb026145124186b5b2fff5e8bd50ccbffaaa
Alexander Ang <alexanderang.24@gmail.com>
2026-09-02 23:48:47 +0700

    Time: 21 ms (35.12%) | Memory: 107.9 MB (66.67%) - LeetSync

--- problems/217-contains-duplicate/contains-duplicate.java
+++ problems/217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            list.add(nums[i]);
-        }
-
         HashSet<Integer> set = new HashSet<>();
-        set.addAll(list);
 
-        return list.size() != set.size();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
     }
 }
