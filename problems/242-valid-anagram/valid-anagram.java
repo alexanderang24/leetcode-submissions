fffb8ded51e4f6d961d268c39b3aefddab591b83
Alexander Ang <alexanderang.24@gmail.com>
2026-09-05 00:19:53 +0700

    Time: 19 ms (10.57%) | Memory: 46.6 MB (25.22%) - LeetSync

--- problems/242-valid-anagram/valid-anagram.java
+++ problems/242-valid-anagram/valid-anagram.java
@@ -1,30 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
         HashMap<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++) {
-            if (map.get(s.charAt(i)) != null) {
-                int val = map.get(s.charAt(i));
-                map.put(s.charAt(i), ++val);
-                // System.out.println("+char: " + s.charAt(i) + " val: " + val);
-            } else {
-                map.put(s.charAt(i), 1);
-                // System.out.println("+char: " + s.charAt(i) + " val: " + 1);
-            }
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
 
         for (int i = 0; i < t.length(); i++) {
-            if (map.get(t.charAt(i)) != null && map.get(t.charAt(i)) > 0) {
-                int val = map.get(t.charAt(i));
-                map.put(t.charAt(i), --val);
-                // System.out.println("-char: " + t.charAt(i) + " val: " + val);
+            char c = t.charAt(i);
+            if (map.containsKey(c) && map.get(c) > 0) {
+                map.put(c, map.get(c) - 1);
             } else {
-                return false;
-            }
-        }
-
-        for (int i = 0; i < map.size(); i++) {
-            if (map.get(s.charAt(i)) != 0) {
-                // System.out.println("not null: " + s.charAt(i) + " val: " + map.get(s.charAt(i)));
                 return false;
             }
         }
