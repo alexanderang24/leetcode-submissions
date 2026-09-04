42c16778b9ae35a9ce9a29ecaee3878d54c11f8e
Alexander Ang <alexanderang.24@gmail.com>
2026-09-05 00:20:53 +0700

    Time: 17 ms (16.92%) | Memory: 46.7 MB (16.19%) - LeetSync

--- problems/242-valid-anagram/valid-anagram.java
+++ problems/242-valid-anagram/valid-anagram.java
@@ -4,7 +4,8 @@ class Solution {
 
         HashMap<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
         for (int i = 0; i < t.length(); i++) {
