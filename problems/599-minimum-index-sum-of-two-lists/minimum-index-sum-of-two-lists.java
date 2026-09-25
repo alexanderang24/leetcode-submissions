2facc9b2e56c2b69fd19ae4ba4a48f5a02e69613
Alexander Ang <alexanderang.24@gmail.com>
2026-09-25 20:36:02 +0700

    Time: 9 ms (48.73%) | Memory: 48 MB (13.01%) - LeetSync

--- problems/599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
+++ problems/599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
@@ -3,7 +3,7 @@ class Solution {
         Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < list1.length; i++) {
             map.put(list1[i], i);
-            System.out.println("added to map: " + list1[i] + " on index " + i);
+            // System.out.println("added to map: " + list1[i] + " on index " + i);
         }
 
         List<String> list = new ArrayList<>();
@@ -11,17 +11,17 @@ class Solution {
         for (int i = 0; i < list2.length; i++) {
             String s = list2[i];
             if (map.containsKey(s)) {
-                System.out.println("map contains " + s);
+                // System.out.println("map contains " + s);
                 int temp = map.get(s) + i;
-                System.out.println("temp: " + temp + " indexSum: " + indexSum);
+                // System.out.println("temp: " + temp + " indexSum: " + indexSum);
                 if (temp == indexSum) {
                     list.add(s);
-                    System.out.println("added to list: " + s);
+                    // System.out.println("added to list: " + s);
                 } else if (temp < indexSum) {
                     indexSum = temp;
                     list = new ArrayList<>();
                     list.add(s);
-                    System.out.println("replaced to list: " + s);
+                    // System.out.println("replaced to list: " + s);
                 }
             }
         }
